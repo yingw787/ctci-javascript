1ec46b0eb252e995025575a750ed612c786bf54a
Ying Wang <yingw787@gmail.com>
2016-10-11 21:38:59 -0400

    Solution skeleton for Chapter 16/Problem 25, Incomplete Solution to Chapter 16/Problem 26

---
+++ CTCI_5/Chapter_16_Moderate/Problem_26.js
@@ -0,0 +1,20 @@
+/*
+    Given an arithmetic equation consisting of positive integers, +, -, *, and / (no parentheses), compute the result.
+
+    EXAMPLE
+
+    Input: 2 * 3 + 5 / 6 * 3 + 15
+    Output: 23.5
+*/
+'use strict';
+
+const assert = require('assert');
+
+//
+function chapterSixteenProblemTwentySixBruteForceSolution(expression) {
+    assert(typeof expression === 'string');
+}
+
+module.exports = {
+    chapterSixteenProblemTwentySixBruteForceSolution: chapterSixteenProblemTwentySixBruteForceSolution,
+};
