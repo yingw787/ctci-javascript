2ccdb344ac6fc5df7d373d220554be329fd18646
Ying Wang <yingw787@gmail.com>
2016-10-20 23:07:12 -0400

    Added and tested book solution for Chapter 16/Problem 6

--- CTCI_5/Chapter_16_Moderate/Problem_6.js
+++ CTCI_5/Chapter_16_Moderate/Problem_6.js
@@ -4,6 +4,7 @@
 'use strict';
 
 const assert = require('assert');
+const MergeSort = require('../Chapter_VII_Technical_Questions/ArrayOfNumbers_MergeSort');
 
 // O(N * M), N = length of arrayOfIntegersOne, M = length of arrayOfIntegersTwo
 // O(1) space
@@ -23,6 +24,47 @@ function chapterSixteenProblemSixBruteForceSolution(arrayOfIntegersOne, arrayOfI
     return difference;
 }
 
+// O(A * log A + B * log B) time, A = length of arrayOfIntegersOne, B = arrayOfIntegersTwo
+// O(A + B) space
+function chapterSixteenProblemSixBookSolution(arrayOfIntegersOne, arrayOfIntegersTwo) {
+    _assertArrayOfIntegers(arrayOfIntegersOne);
+    _assertArrayOfIntegers(arrayOfIntegersTwo);
+
+    let sortedArrayOfIntegersOne = new MergeSort(arrayOfIntegersOne).sortByAscendingOrder();
+    if (sortedArrayOfIntegersOne.length === 0) {
+        return null;
+    }
+    const sortedArrayOfIntegersTwo = new MergeSort(arrayOfIntegersTwo).sortByAscendingOrder();
+
+    let a = 0;
+    let b = 0;
+
+    let minimumDifference = Infinity;
+
+    while (a < sortedArrayOfIntegersOne.length && b < sortedArrayOfIntegersTwo.length) {
+        if (Math.abs(sortedArrayOfIntegersOne[a] - sortedArrayOfIntegersTwo[b]) < minimumDifference) {
+            minimumDifference = Math.abs(sortedArrayOfIntegersOne[a] - sortedArrayOfIntegersTwo[b]);
+        }
+
+        if (sortedArrayOfIntegersOne[a] < sortedArrayOfIntegersTwo[b]) {
+            a++;
+        } else {
+            b++;
+        }
+    }
+
+    return minimumDifference;
+}
+
+function _assertArrayOfIntegers(arrayOfIntegers) {
+    assert(typeof arrayOfIntegers === 'object');
+    for (let i = 0; i < arrayOfIntegers.length; i++) {
+        const value = arrayOfIntegers[i];
+        assert(typeof value === 'number' && value % 1 === 0);
+    }
+}
+
 module.exports = {
     chapterSixteenProblemSixBruteForceSolution: chapterSixteenProblemSixBruteForceSolution,
+    chapterSixteenProblemSixBookSolution: chapterSixteenProblemSixBookSolution,
 };
--- test/Chapter_16_Problem_6.js
+++ test/Chapter_16_Problem_6.js
@@ -18,3 +18,18 @@ describe('chapterSixteenProblemSixBruteForceSolution', function() {
         assert.equal(Problem_6.chapterSixteenProblemSixBruteForceSolution(arrayOfIntegersOne, arrayOfIntegersTwo), 0);
     });
 });
+
+describe('chapterSixteenProblemSixBookSolution', function() {
+    it('should throw error when given wrong type', function() {
+        const arrayOfIntegersOne = 5;
+        const arrayOfIntegersTwo = 'hi';
+        expect(function() {
+            Problem_6.chapterSixteenProblemSixBookSolution(arrayOfIntegersOne, arrayOfIntegersTwo);
+        }).to.throw(Error);
+    });
+    it('should return 0 for [1, 5, 8, 9, 10, 14, 89] and [89, 2, 7, 11, 12, 78]', function() {
+        const arrayOfIntegersOne = [1, 5, 8, 9, 10, 14, 89];
+        const arrayOfIntegersTwo = [89, 2, 7, 11, 12, 78];
+        assert.equal(Problem_6.chapterSixteenProblemSixBookSolution(arrayOfIntegersOne, arrayOfIntegersTwo), 0);
+    });
+});
