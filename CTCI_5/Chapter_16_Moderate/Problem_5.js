bfd907fa431957373f0110da2fa0208b018d22d1
Ying Wang <yingw787@gmail.com>
2016-10-20 22:29:54 -0400

    Added and tested improved solution to Chapter 16/Problem 5

--- CTCI_5/Chapter_16_Moderate/Problem_5.js
+++ CTCI_5/Chapter_16_Moderate/Problem_5.js
@@ -16,7 +16,7 @@ function factorial(n) {
 // O(N) space
 // REDO need to be more efficient
 function chapterSixteenProblemFiveBruteForceSolution(n) {
-    assert(typeof n === 'number');
+    assert(typeof n === 'number' && n % 1 === 0 && n > 0);
     if (n % 1 !== 0 || n < 1) {
         throw new Error('n must be a positive integer.');
     }
@@ -31,6 +31,43 @@ function chapterSixteenProblemFiveBruteForceSolution(n) {
     return i;
 }
 
+// O(N * K) time, N = n, K = largest number of factors of either 2 or 5
+// O(1) space
+// CORRECT NO REDO
+function chapterSixteenProblemFiveImprovedSolution(n) {
+    // count the number of factors of five and two
+    assert(typeof n === 'number' && n % 1 === 0 && n > 0);
+    let numberOfFactorsOfTwo = 0;
+    let numberOfFactorsOfFive = 0;
+
+    for (let i = 1; i <= n; i++) {
+        const value = i;
+        numberOfFactorsOfTwo += _calculateNumberOfFactorsOfTwo(value);
+        numberOfFactorsOfFive += _calculateNumberOfFactorsOfFive(value);
+    }
+
+    return Math.min(numberOfFactorsOfTwo, numberOfFactorsOfFive);
+}
+
+function _calculateNumberOfFactorsOfTwo(value) {
+    let numberOfFactorsOfTwo = 0;
+    while (value % 2 === 0) {
+        numberOfFactorsOfTwo++;
+        value /= 2;
+    }
+    return numberOfFactorsOfTwo;
+}
+
+function _calculateNumberOfFactorsOfFive(value) {
+    let numberOfFactorsOfFive = 0;
+    while (value % 5 === 0) {
+        numberOfFactorsOfFive++;
+        value /= 5;
+    }
+    return numberOfFactorsOfFive;
+}
+
 module.exports = {
     chapterSixteenProblemFiveBruteForceSolution: chapterSixteenProblemFiveBruteForceSolution,
+    chapterSixteenProblemFiveImprovedSolution: chapterSixteenProblemFiveImprovedSolution,
 };
--- test/Chapter_16_Problem_5.js
+++ test/Chapter_16_Problem_5.js
@@ -30,3 +30,34 @@ describe('chapterSixteenProblemFiveBruteForceSolution', function() {
         assert.equal(Problem_5.chapterSixteenProblemFiveBruteForceSolution(numberOne), 1);
     });
 });
+
+describe('chapterSixteenProblemFiveImprovedSolution', function() {
+    // it('should throw error when given wrong type', function() {
+    //     const numberOne = 'hi';
+    //     expect(function() {
+    //         Problem_5.chapterSixteenProblemFiveImprovedSolution(numberOne);
+    //     }).to.throw(Error);
+    //
+    //     const numberTwo = 1.5;
+    //     expect(function() {
+    //         Problem_5.chapterSixteenProblemFiveImprovedSolution(numberTwo);
+    //     }).to.throw(Error);
+    //
+    //     const numberThree = -5;
+    //     expect(function() {
+    //         Problem_5.chapterSixteenProblemFiveImprovedSolution(numberThree);
+    //     }).to.throw(Error);
+    // });
+    // it('should return 0 for n = 4', function() {
+    //     const numberOne = 4;
+    //     assert.equal(Problem_5.chapterSixteenProblemFiveImprovedSolution(numberOne), 0);
+    // });
+    it('should return 2 for n = 10', function() {
+        const numberOne = 10;
+        assert.equal(Problem_5.chapterSixteenProblemFiveImprovedSolution(numberOne), 2);
+    });
+    it('should return 4 for n = 23', function() {
+        const numberOne = 23;
+        assert.equal(Problem_5.chapterSixteenProblemFiveImprovedSolution(numberOne), 4);
+    });
+});
